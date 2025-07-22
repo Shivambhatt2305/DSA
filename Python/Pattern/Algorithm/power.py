d72b28ff36c18d7db8085158bfccaaf35c56d2d2
Shivam Bhatt <143023404+Shivambhatt2305@users.noreply.github.com>
2025-07-22 08:22:27 +0530

    Update power.py

--- Python/Pattern/Algorithm/power.py
+++ Python/Pattern/Algorithm/power.py
@@ -5,3 +5,12 @@ def power(base, exponent):
     return base
 
 print(power(2, 3))  
+
+#2nd approch using recursion
+def power(base,exponent):
+    if exponent == 0:
+        return 1
+    else:
+        return base*power(base,exponent-1)
+
+print(power(2, 3))
