2b577c70ab4524faf2b570f7ee19d81f66940653
Shivam Bhatt <143023404+Shivambhatt2305@users.noreply.github.com>
2025-07-23 09:51:54 +0530

    Add files via upload

--- Python/Pattern/Algorithm/power.py
+++ Python/Pattern/Algorithm/power.py
@@ -1,10 +1,10 @@
-def power(base, exponent):
-    a=base
-    for i in range(exponent-1):
-        base=base*a
-    return base
+# def power(base, exponent):
+#     a=base
+#     for i in range(exponent-1):
+#         base=base*a
+#     return base
 
-print(power(2, 3))  
+# print(power(2, 3))  
 
 #2nd approch using recursion
 def power(base,exponent):
